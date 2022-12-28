b321a77b7ae6616dda775ff80fd20118f4963c7d
Mikhail Lyalin <mc.lyalin@gmail.com>
2022-12-28 23:35:54 +0500

    Override __init__

--- cat.py
+++ cat.py
@@ -4,16 +4,19 @@ class Cat:
     self.gender = gender
     self.age = age
 
+  def __str__(self):
+    return f'имя: {self.name}\tпол: {self.gender}\tвозраст: {self.age}'
+
   # def init_from_dict(self, cat_dict={}):
   #   self.name = cat_dict.get('name')
-  #   self.breed = cat_dict.get('breed')
+  #   self.gender = cat_dict.get('gender')
   #   self.age = cat_dict.get('age')
 
-  def getName(self):
-    return self.name
+  # def getName(self):
+  #   return self.name
 
-  def getGender(self):
-    return self.gender
+  # def getGender(self):
+  #   return self.gender
 
-  def getAge(self):
-    return self.age
+  # def getAge(self):
+  #   return self.age
